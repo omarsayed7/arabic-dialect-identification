2dc68250cfd7fdd656680d911011376859c8a18b
omarsayed7 <sayedomar74@gmail.com>
2022-03-12 16:46:47 +0200

    Updating the structure of the flask app

--- model_deploy.py
+++ model_deploy.py
@@ -1,16 +1,25 @@
-from flask import Flask, render_template
-from endpoints import blueprint as classification_endpoint
-from flask_ngrok import run_with_ngrok
+from flask import Flask, render_template, make_response, request
 from flask_cors import CORS
-
+from src.text_inference_deploy import select_model
 app = Flask(__name__)
 CORS(app)
-app.register_blueprint(classification_endpoint)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''Post method for arabic dialect classification'''
+    data = request.form
+    input_text = data['InputText']
+    algorithm = data['Algorithm']
+    pred_class = select_model(algorithm, input_text)
+    print(pred_class)
+    headers = {'Content-Type': 'text/html'}
+    return make_response(render_template('result.html', result=pred_class), 200, headers)
 
 
 if __name__ == '__main__':
